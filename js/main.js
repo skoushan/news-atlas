1eaf802fad38ed46cf9396f1f77cc5fceeb748b5
skoushan <soheil.koushan@gmail.com>
2014-01-24 22:56:24 -0500

    HTML5 boilerplate

---
+++ js/main.js
@@ -0,0 +1,93 @@
+google.load("feeds", "1"); //Load Google Ajax Feed API (version 1)		
+var geocoder;
+var map;
+var address;
+google.maps.visualRefresh = true;
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var mapOptions = {
+        center: new google.maps.LatLng(-20, 0),
+        zoom: 2,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    //			var strictBounds = new google.maps.LatLngBounds(
+    //			new google.maps.LatLng(85, -180),
+    //		new google.maps.LatLng(-85, 180)
+    //);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+function codeAddress() {
+    address = document.getElementById('address').value;
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+
+            var contentString = '<div id="content">' +
+                '<div id="siteNotice">' +
+                '</div>' +
+                '<h1 id="firstHeading" class="firstHeading">TEST					</h1>' +
+                '<div id="bodyContent">' +
+                '<p>testasdfasdfasdf</p>' +
+                '</div>' +
+                '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                animation: google.maps.Animation.DROP
+            });
+
+            var flag = 1;
+            google.maps.event.addListener(marker, 'click', function () {
+                if (flag == 1) {
+                    infowindow.open(map, marker);
+                    map.setCenter(map.center);
+                    title: address;
+                    flag = 0;
+                } else {
+                    infowindow.close(map, marker);
+                    map.setCenter(map.center);
+                    flag = 1;
+                }
+            });
+        } else {
+            alert("Geocode was not successful: " + status);
+        }
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+var feedcontainer = document.getElementById("feeddiv")
+var feedurl = "http://feeds.reuters.com/Reuters/worldNews"
+var feedlimit = 20
+var rssoutput = "<b>Latest World News</b><br /><ul>"
+
+    function rssfeedsetup() {
+        var feedpointer = new google.feeds.Feed(feedurl) //Google Feed API method
+        feedpointer.setNumEntries(feedlimit) //Google Feed API method
+        feedpointer.load(displayfeed) //Google Feed API method
+    }
+
+    function displayfeed(result) {
+        if (!result.error) {
+            var thefeeds = result.feed.entries
+            for (var i = 0; i < thefeeds.length; i++)
+                rssoutput += "<li><a href='" + thefeeds[i].link + "'>" + thefeeds[i].title + thefeeds[i].content.substr(0, thefeeds[i].content.indexOf("(Reuters)") - 1) + "</a></li>"
+            rssoutput += "</ul>"
+            feedcontainer.innerHTML = rssoutput
+        } else
+            alert("Error fetching feeds!")
+    }
+
+window.onload = function () {
+    rssfeedsetup()
+}
